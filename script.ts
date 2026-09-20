15858692fea486c776eec7236fe82f5e9e425f12
GitTanish <tanishsaroj6781@gmail.com>
2026-09-20 23:34:44 +0530

    document TypeScript primitive and reference types

--- script.ts
+++ script.ts
@@ -1,1 +1,13 @@
-var a= 10;
+// basic types
+// primitives( number, string, boolean, null, undefined, symbol, bigint)
+// Arrays
+// Tuples
+// Enums
+// Any, Unknown, Never, Void, Undefined, Null
+
+// primitives and reference
+// [] {} () -> reference types
+
+let a= 12;
+let arr = [1, 2, 3, 4, "hello",{key: "value"}];
+let arr2: number[] = [1, 2, 3, 4];
