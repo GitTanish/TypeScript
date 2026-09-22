24c9d89270463429f0b72bf79e960b1a0ee082cd
GitTanish <tanishsaroj6781@gmail.com>
2026-09-23 00:19:47 +0530

    Inference and Type Annotation

--- script.ts
+++ script.ts
@@ -12,3 +12,54 @@
 let arr: [string, number] = ["Orange", 22];
 
 let arr2:[number, string]=[19, "Apple"];
+
+//enumerations
+enum UserRole {
+    ADMIN= "admin",
+    GUEST= "guest",
+    SUPER_ADMIN= "super_admin"
+}
+
+UserRole.ADMIN; // "admin"
+
+enum StatusCode {
+    ABANDONED ='abandoned status code 500',
+    NOTFOUND = 'not_found status code 404',
+}
+
+StatusCode.NOTFOUND; // "abandoned status code 500"
+
+let a: number;
+a=88;
+let b; // any type
+let c: unknown;
+
+if(typeof c === 'string'){
+    c.toUpperCase();
+};
+
+
+//void <- returns nothing
+function abdc():void{
+    console.log('hey')
+}
+
+//
+function diablo():string{
+    return 'hello'
+}
+
+//null
+let j:null;
+let k: null | string;
+k='cat';
+
+//never
+function jklm():never{
+    while(true){
+        console.log('hello')
+    }
+}
+
+//jklm(); // this function will never return anything, it will run forever
+console.log('this will never be printed') // this line will never be executed
---
+++ script2.ts
@@ -0,0 +1,11 @@
+// Type Inference
+// Understanding type inference
+// type annotations
+
+let a: number = 10; // inferred as number
+
+let b: string = "Hello"; // b:string is type annotated as string
+
+let c: boolean;
+
+function abcd(a: number, b:string): void {}
